3bcb0847f31e369c67a6f4bac1f2cabc239447d7
Cole Batson <cbatson89@gmail.com>
2016-11-02 23:00:20 -0600

    began working on exercise

--- boxes.js
+++ boxes.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+$(document).ready(function() {
+  console.log("roger that");
+$("#secretBox").css("background-color", "white");
+$("#secretBox").append("<h1>secret box!</h1>");
+$("#row1").children().attr("class", "box boxType3");
+$(".box").eq(-1).css("display","none");
+$(".boxType1").css("background-color", "white");
+$("#row2, div:nth-child(0+1)").css("background-color", "black")
+
+});
